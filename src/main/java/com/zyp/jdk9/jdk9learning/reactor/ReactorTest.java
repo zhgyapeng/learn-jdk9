6f808791559ab02c1725c4d84831b09591b2ef8f
zhgyapeng <zhgyapeng@users.noreply.github.com>
2019-07-23 11:30:13 +0800

    Update ReactorTest.java

--- src/main/java/com/zyp/jdk9/jdk9learning/reactor/ReactorTest.java
+++ src/main/java/com/zyp/jdk9/jdk9learning/reactor/ReactorTest.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * @author zhangyp 01385499
+ * @author zhangyp
  */
 public class ReactorTest {
     public static void main(String[] args) {
